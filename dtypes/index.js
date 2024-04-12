9078a112dc5fd09f9f5055da119d2c08253dca38
kennethDev <117411362+Kennethcpp@users.noreply.github.com>
2024-04-12 17:15:09 +0100

    Add files via upload

---
+++ dtypes/index.js
@@ -0,0 +1,94 @@
+
+//CREATING A STRING DATATYPE
+//1
+const myNameIsNiceOne = "kenneth onyeonoro"
+console.log(myNameIsNiceOne)
+//2
+const amLearningHowToCode = "javaScript"
+console.log(amLearningHowToCode)
+//3
+const theNameOfMyMentor = "Sam_Dev"
+console.log(theNameOfMyMentor)
+//4
+const myFatherNameMySister = "Miracle"
+console.log(myFatherNameMySister)
+//5 
+ const myFavoriteFoodIsHere = "Rice"
+console.log(myFavoriteFoodIsHere)
+
+//CREATING A NUMBER DATATYPE
+//1
+
+var thisIsMyNumberVariable = 212237
+console.log(thisIsMyNumberVariable)
+//2
+var thisNumberIsNotEen = 27
+console.log(thisNumberIsNotEen)
+//3
+var thisNumberIsEvenNum = 20
+console.log(thisNumberIsEvenNum)
+//4
+var theBasicNumberOperationIs = 1234567890
+console.log(theBasicNumberOperationIs)
+//5
+var thisIsLastNumberVariable = 90
+console.log(thisIsLastNumberVariable)
+
+//creating a null DATATYPE
+//1
+const theFisrtDataTypeNull = null
+console.log(theFisrtDataTypeNull)
+//2
+const howDoIUseNulltype = null
+console.log(howDoIUseNulltype) 
+//3
+const toStoreErrorTransactionProb = null
+console.log(toStoreErrorTransactionProb)
+//4
+const myFavoriteDataTypeIs = null
+console.log(myFavoriteDataTypeIs)
+//5
+const iHaveSomeNullContainers = null
+console.log(iHaveSomeNullContainers)
+
+
+//CREATING A BOOLEAN DATATYPE
+//1
+const thisIsMyFirstBool = true
+console.log(thisIsMyFirstBool)
+//2
+const iHaveBoughtManyCar = false
+console.log(iHaveBoughtManyCar)
+//3
+const isMyFatherRealyDead = false
+console.log(isMyFatherRealyDead)
+//4
+const myNameIsNotKenneth = false
+console.log(myNameIsNotKenneth)
+//5
+const LearningHowToCodejs = true
+console.log(LearningHowToCodejs)
+
+//CREATING AN UNDIFINED DATATYPE and later defined.
+//1
+let myNameIsKennethOnyeonoro = "" //undefined
+
+ myNameIsKennethOnyeonoro = "True" //defined
+console.log(myNameIsKennethOnyeonoro)
+//2
+let myBestFoodBeThis = ""
+myBestFoodBeThis = "Rice"
+//3
+let isTheNameGoodName = ""
+isTheNameGoodName = "Yes"
+console.log(isTheNameGoodName)
+//4
+let myFatherMotherIsAunt = ""
+myFatherMotherIsAunt = "No"
+console.log(myFatherMotherIsAunt)
+//5
+let thisIsAnUndefinedVariable = ""
+thisIsAnUndefinedVariable = "Yes"
+console.log(thisIsAnUndefinedVariable)
+
+
